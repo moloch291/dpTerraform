62dbe4835f53a2e93496370f6efe8ab208138dd3
moloch291 <danielkomjati1@gmail.com>
2022-02-11 17:13:03 +0100

    refactor svc and svc_plan names

--- drport_resource_group/main.tf
+++ drport_resource_group/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_mssql_database" "mssql_db" {
 ##### App service: #####
 resource "azurerm_app_service_plan" "drp_svc_plan" {
   depends_on          = [azurem_resource_group.rg]
-  name                = "example-appserviceplan"
+  name                = "drp_svc_plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "${var.app_svc_plan_kind}"
@@ -71,9 +71,9 @@ resource "azurerm_app_service_plan" "drp_svc_plan" {
   }
 }
 
-resource "azurerm_app_service" "example" {
+resource "azurerm_app_service" "drp_svc" {
   depends_on          = [azurerm_app_service_plan.drp_svc_plan]
-  name                = "example-app-service"
+  name                = "drp_svc"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.drp_svc_plan.id
